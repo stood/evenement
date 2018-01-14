29a50b05e1b6e1ee51feafb7cab40d0315f1fdb6
Mikael Paris <stood86@gmail.com>
2018-01-14 20:50:20 +0100

    add js for collection

---
+++ public/js/add-collection-widget.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function () {
+    jQuery('.add-another-collection-widget').click(function (e) {
+        e.preventDefault();
+        var list = jQuery(jQuery(this).attr('data-list'));
+
+        // Try to find the counter of the list
+        var counter = list.data('widget-counter') | list.children().length;
+        // If the counter does not exist, use the length of the list
+        if (!counter) { counter = list.children().length; }
+
+        // grab the prototype template
+        var newWidget = list.data('prototype');
+        // replace the "__name__" used in the id and name of the prototype
+        // with a number that's unique to your emails
+        // end name attribute looks like name="contact[emails][2]"
+        newWidget = newWidget.replace(/__name__/g, counter);
+        // Increase the counter
+        counter++;
+        // And store it, the length cannot be used if deleting widgets is allowed
+        list.data(' widget-counter', counter);
+
+        // create a new list element and add it to the list
+        var newElem = jQuery(list.attr('data-widget-tags')).html(newWidget);
+        newElem.appendTo(list);
+    });
+});
